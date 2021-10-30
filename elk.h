ad796555de3a53a2efe3aad10f3e0df209983094
Sergey Lyubka <valenok@gmail.com>
2021-10-30 17:22:14 +0100

    2.1.2

--- elk.h
+++ elk.h
@@ -17,7 +17,7 @@
 #pragma once
 #include <stdint.h>
 
-#define JS_VERSION "2.1.1"
+#define JS_VERSION "2.1.2"
 
 struct js;                 // JS engine (opaque)
 typedef uint64_t jsval_t;  // JS value placeholder
